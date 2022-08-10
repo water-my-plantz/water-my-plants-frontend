3cfbb956e5398eeea5978b778e7542b2b3dd8170
Chase Donovan <chase.donovan@aol.com>
2022-08-10 10:10:04 -0700

    👍 IMPROVE: delete doesnt reload page

--- src/components/plants/plant.js
+++ src/components/plants/plant.js
@@ -1,19 +1,23 @@
 
-import React from "react";
+import React, { useContext } from "react";
 import "../../App.css"
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { GlobalPropsContext } from "../GlobalPropsContext";
+
 
 export default function Plant({ plant }) {
     const { image, nickname, species, water_frequency } = plant;
     const plantId = plant.id;
+    const { setPlantList, plantList } = useContext(GlobalPropsContext);
+
 
     //MAKE DELETE WORK**
     const handleDelete = () => {
         axios
             .delete(`https://water-my-plants-fullstack-api.herokuapp.com/plants/${plantId}`) // Endpoint to get all plants in Database
             .then((res) => {
-                window.location.reload();
+                setPlantList(plantList.filter(plant => plant.id !== plantId));
             })
             .catch((err) => {
                 console.error("Server Error", err);
